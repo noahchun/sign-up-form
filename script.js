8154e608349c18cd8f2da43f93328341b7bda2d5
Noah <noahchun77@gmail.com>
2023-07-16 14:41:37 -0400

    animations for form

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', function() {
     const imgContainer = document.querySelector('.imgContainer');
+    const formContainer = document.querySelector('.formContainer');
     imgContainer.classList.add('animation');
+    formContainer.classList.add('animation');
 });
