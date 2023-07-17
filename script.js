9fb15fd6f7edd50a75da0037f4402296280a5088
Noah <noahchun77@gmail.com>
2023-07-16 20:43:33 -0400

    header transition

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 window.addEventListener('load', function() {
     const imgContainer = document.querySelector('.imgContainer');
+    const textHeader = document.querySelector('.textHeader');
     const formContainer = document.querySelector('.formContainer');
     imgContainer.classList.add('animation');
+    textHeader.classList.add('dropAnimation');
     formContainer.classList.add('animation');
 });
+
+var password = document.getElementById("password");
+var confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+    if(password.value != confirm_password.value) {
+      confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+      confirm_password.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
